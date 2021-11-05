92aa2fa0c32b44c025689d3f0d484b6ccb78d019
jodh <jodh@itu.dk>
2021-11-05 23:27:59 +0100

    created src/values/colors.js
    custom defined colors, babyy~

---
+++ scan-car/src/values/colors.js
@@ -0,0 +1,32 @@
+/**------------ScanCar Palette Colors ------------ */
+
+/**Based on the custom defined colors ReactJS by Sujar Kumar, June 29th 2020
+https://medium.com/@suraj27/global-colors-in-reactjs-custom-defined-3d4b76efa4fc
+
+To use the colors by name, you need to...
+
+1..... go into the file you want to use the custom colors in
+
+2..... add the following to the imports:
+    import { COLORS } from "src/values/colors.js"
+    .. or whatever path you have to the file from where your component file is located
+
+3.... use COLORS.Color000 to call the color you need. Example:
+    COLORS.Blue300 
+    .. this will grab the string "#4B7DB9" and insert it at that point.
+
+*/
+
+/** */
+
+export const COLORS = {
+    Blue700: "#20334B",
+    Blue600: "#31465F",
+    Blue500: "#355C8A",
+    Blue400: "#3D6A9E",
+    Blue300: "#4B7DB9",
+    Blue200: "#9FBADB",
+    Blue100: "#F0F4F9",
+
+
+}
