3a4e76665453f3169b681399dfe735cebffebef8
jodh <jodh@itu.dk>
2021-11-07 12:25:03 +0100

    Fixed transfers to use CARS and LOCATIONS import

--- scan-car/src/data/transfer.js
+++ scan-car/src/data/transfer.js
@@ -1,9 +1,12 @@
+import CARS from "cars.js"
+import LOCATIONS from "location.js"
+
 export let TRANSFERS = {
-    1: {Incoming:"CPC", Outgoing:"CPA", Date:"25.11.2021", Car:"JF94837"},
-    2: {Incoming:"CPC", Outgoing:"CPA", Date:"25.11.2021", Car:"FM23456"},
-    3: {Incoming:"CPA", Outgoing:"CPC", Date:"25.11.2021", Car:"IH12345"},
-    4: {Incoming:"AHN", Outgoing:"AHA", Date:"25.11.2021", Car:"HM23456"},
-    5: {Incoming:"AHA", Outgoing:"AHC", Date:"25.11.2021", Car:"HM12345"},
-    6: {Incoming:"AHA", Outgoing:"AHS", Date:"25.10.2021", Car:"IH23456"},
-    7: {Incoming:"AHS", Outgoing:"AHN", Date:"26.10.2021", Car:"FC56789"}
+    1: {Incoming:LOCATIONS.CPC, Outgoing:LOCATIONS.CPA, Date:"25.11.2021", Car:CARS.JF94837},
+    2: {Incoming:LOCATIONS.CPC, Outgoing:LOCATIONS.CPA, Date:"25.11.2021", Car:CARS.FM23456},
+    3: {Incoming:LOCATIONS.CPA, Outgoing:LOCATIONS.CPC, Date:"25.11.2021", Car:CARS.IH12345},
+    4: {Incoming:LOCATIONS.AHN, Outgoing:LOCATIONS.AHA, Date:"25.11.2021", Car:CARS.HM23456},
+    5: {Incoming:LOCATIONS.AHA, Outgoing:LOCATIONS.AHC, Date:"25.11.2021", Car:CARS.HM12345},
+    6: {Incoming:LOCATIONS.AHA, Outgoing:LOCATIONS.AHS, Date:"25.10.2021", Car:CARS.IH23456},
+    7: {Incoming:LOCATIONS.AHS, Outgoing:LOCATIONS.AHN, Date:"26.10.2021", Car:CARS.FC56789}
 }
