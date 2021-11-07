f8b7c62fc50376d3f2e14a1f87a84729a5e573e0
jodh <jodh@itu.dk>
2021-11-07 12:19:34 +0100

    Added transfer.js

---
+++ scan-car/src/data/transfer.js
@@ -0,0 +1,9 @@
+export let TRANSFERS = {
+    1: {Incoming:"CPC", Outgoing:"CPA", Date:"25.11.2021", Car:"JF94837"},
+    2: {Incoming:"CPC", Outgoing:"CPA", Date:"25.11.2021", Car:"FM23456"},
+    3: {Incoming:"CPA", Outgoing:"CPC", Date:"25.11.2021", Car:"IH12345"},
+    4: {Incoming:"AHN", Outgoing:"AHA", Date:"25.11.2021", Car:"HM23456"},
+    5: {Incoming:"AHA", Outgoing:"AHC", Date:"25.11.2021", Car:"HM12345"},
+    6: {Incoming:"AHA", Outgoing:"AHS", Date:"25.10.2021", Car:"IH23456"},
+    7: {Incoming:"AHS", Outgoing:"AHN", Date:"26.10.2021", Car:"FC56789"}
+}
