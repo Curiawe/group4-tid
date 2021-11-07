9c8e768708eb0f0f97a05793d5ff3e6b876e2a7f
jodh <jodh@itu.dk>
2021-11-07 13:49:06 +0100

    added walkins, updated bookings

---
+++ scan-car/src/data/walkins.js
@@ -0,0 +1,20 @@
+export let WALKINS = {
+    1: {Date: Date.UTC(2021,10,16), estimate: 2, total: 2, changeable: false},
+    2: {Date: Date.UTC(2021,10,17), estimate: 3, total: 3, changeable: false},
+    3: {Date: Date.UTC(2021,10,18), estimate: 3, total: 4, changeable: false},
+    4: {Date: Date.UTC(2021,10,19), estimate: 5, total: 5, changeable: false},
+    5: {Date: Date.UTC(2021,10,20), estimate: 7, total: 6, changeable: false},
+    6: {Date: Date.UTC(2021,10,21), estimate: 2, total: 2, changeable: false},
+    7: {Date: Date.UTC(2021,10,22), estimate: 6, total: 0, changeable: true},
+    8: {Date: Date.UTC(2021,10,23), estimate: 3, total: 0, changeable: true},
+    9: {Date: Date.UTC(2021,10,24), estimate: 1, total: 0, changeable: true},
+    10: {Date: Date.UTC(2021,10,25), estimate: 0, total: 0, changeable: true},
+    11: {Date: Date.UTC(2021,10,26), estimate: 0, total: 0, changeable: true},
+}
+
+// logic required:
+/**
+ * if date = today, set changeable to false
+ * find a way to automate dates?
+ * 
+ */
