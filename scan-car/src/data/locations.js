b42f3d476f534cbdb29dc241114fb90cfab665bc
jodh <jodh@itu.dk>
2021-11-07 11:47:19 +0100

    Locations file added

---
+++ scan-car/src/data/locations.js
@@ -0,0 +1,15 @@
+/**Schema:
+ * LOC: ["Location Name", "Address", "Email", "Phone","Region"]
+ */
+
+export let LOCATIONS = {
+    CPC:["CPH City Center","Frørupvej 18, 1154 Københaven","CPC@scancar.dk", "00 00 00 00", "Hovedstaden"],
+    CPA:["CPH Airport", "Lufthavnsboulevarden 2, 2770 Kastrup","CPA@scancar.dk", "11 11 11 11", "Hovedstaden"],
+    AHC:["Aarhus Central", "Bygholms Alle 10, 8260 Viby J","AHC@scancar.dk", "22 22 22 22","Midtjylland"],
+    AHN:["Aarhus North", "Paludan-Müllers Vej 82, 8200 Aarhus N", "AHN@scancar.dk", "33 33 33 33","Midtjylland"],
+    FYS:["Fyn South", "Hal J, Frederiksø 2, 5700 Svendborg","FYA@scancar.dk", "77 77 77 77","Fyn"],
+    FYN:["Fyn North", "Gamle Havnekaj 3, 5000 Odense C","FYN@scancar.dk", "66 66 66 66","Fyn"],
+    AHS:["Aarhus South", "Moesgårdvej 11B, 8270 Højbjerg","AHS@scancar.dk", "44 44 44 44","Midtjylland"],
+    AHA:["Aarhus Harbor", "Værkmestergade 5, 8000 Aarhus","AHA@scancar.dk", "55 55 55 55","Midtjylland"],
+}
+
