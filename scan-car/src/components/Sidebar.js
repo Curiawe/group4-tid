a3c97ec49a587e5460c59bf15b3a5efc10696e1f
jodh <jodh@itu.dk>
2021-11-10 15:48:57 +0100

    got started on sidebars. Files, classes defined

---
+++ scan-car/src/components/Sidebar.js
@@ -0,0 +1,15 @@
+function Sidebar () {
+    return (
+        <div class-name="sidebar">
+{            <div class="sidebarContent">
+                <div class ="sidebarLink">Booking Overview</div>
+                <div class="sidebarLink">Booking Search</div>
+                <div class ="sidebarLink">Car Overview</div>
+                <div class="sidebarLink">Schedule</div>
+            </div>}
+            
+        </div>
+    )
+}
+
+export default Sidebar;
