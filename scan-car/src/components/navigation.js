cc69ca5970b002ff02cc195c81fa6b64acde9138
jodh <jodh@itu.dk>
2021-11-10 17:57:32 +0100

    Trying to fix the styling in the routing

--- scan-car/src/components/navigation.js
+++ scan-car/src/components/navigation.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './navigation.css';
+import { NavLink as Link } from "react-router-dom";
 import logo from './logo.png';
 //import logo from '.\.\public\logo.png';
 
@@ -6,10 +8,10 @@ function Navigation () {
     return (
         <header>
             <div class="navtabs">
-                <div class ="navtab-Logo"><img src={logo} alt="ScanCar Logo" height="40px"></img></div>
-                <div class="navTabActive">Booking Management</div>
-                <div class ="navtab">Rental Management</div>
-                <div class="navtab">Group Management</div>
+                <Link to= "/" class-name="navtab-Logo" activeStyle><img src={logo} alt="ScanCar Logo" height="40px"></img></Link>
+                <div class-name="navTabActive"><Link to="/" activeStyle>Booking Management</Link></div>
+                <Link to="/" class-name="navtab" activeStyle>Rental Management</Link>
+                <Link to="/" class="navtab" activeStyle>Group Management</Link>
             </div>
             
         </header>
--- scan-car/src/index.js
+++ scan-car/src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Navigation from './components/navigation';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navigation />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
