45e1747c47394b6dfa7ac72ffd867c56a6a12f0a
jodh <jodh@itu.dk>
2021-11-10 09:40:34 +0100

    started work

---
+++ scan-car/src/components/sidebar.js
@@ -0,0 +1,17 @@
+/**navSidebar
+  navSidebarContent
+  navSidebarLinks
+ */
+
+import './sidebar.css';
+
+
+function Sidebar() {
+    return (
+        <div class="sidebar">
+            test text
+        </div>
+
+    )};
+
+export default Sidebar();
--- scan-car/src/index.js
+++ scan-car/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sidebar from './components/sidebar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Sidebar />
   </React.StrictMode>,
   document.getElementById('root')
 );
