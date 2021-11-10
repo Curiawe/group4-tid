436eb36e2307c605f874a7530f4239f20a146033
jodh <jodh@itu.dk>
2021-11-10 19:25:18 +0100

    TT-TT

--- scan-car/src/components/navigation.js
+++ scan-car/src/components/navigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './navigation.css';
-import { NavLink as Link } from "react-router-dom";
 import logo from './logo.png';
 //import logo from '.\.\public\logo.png';
 
@@ -8,10 +7,10 @@ function Navigation () {
     return (
         <header>
             <div class="navtabs">
-                <Link to= "/" class-name="navtab-Logo" activeStyle><img src={logo} alt="ScanCar Logo" height="40px"></img></Link>
-                <div class-name="navTabActive"><Link to="/" activeStyle>Booking Management</Link></div>
-                <Link to="/" class-name="navtab" activeStyle>Rental Management</Link>
-                <Link to="/" class="navtab" activeStyle>Group Management</Link>
+                <div class-name="navtab-Logo" activeStyle><img src={logo} alt="ScanCar Logo" height="40px"></img></div>
+                <div class-name="navTabActive">Booking Management</div>
+                <div class-name="navtab" activeStyle>Rental Management</div>
+                <div class="navtab" activeStyle>Group Management</div>
             </div>
             
         </header>
