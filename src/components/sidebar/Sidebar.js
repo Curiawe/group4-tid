8d0de67ae269248a5a254523eba0b91ed61652af
jodh <jodh@itu.dk>
2021-11-12 21:00:25 +0100

    Basic sidebar Structure

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import './sidebar.css';
+
+function Sidebar () {
+    return (
+        <sidebarContainer>
+            <p>Hello</p>
+            <sidebarLinks>
+                <nav>One</nav>
+                <nav>Two</nav>
+                <nav>Three</nav>
+                <nav>Four</nav>
+            </sidebarLinks>
+        </sidebarContainer>
+    )
+}
+
+export default Sidebar;
