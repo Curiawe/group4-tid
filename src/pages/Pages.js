d9ad4dba842001eba07bdff8ee1186a8cfabfc88
jodh <jodh@itu.dk>
2021-11-17 13:24:04 +0100

    Created Pages constant

---
+++ src/pages/Pages.js
@@ -0,0 +1,16 @@
+const Pages = {
+    BookingOverview: "/booking-overview",
+    BookingSearch: "/booking-search",
+    CarGroups: "/car-groups",
+    Cars: "/cars",
+    DailyOverview: "/daily-overview",
+    Home: "/",
+    Pickup: "/pickup",
+    RentalOffices: "/offices",
+    Return: "/return",
+    Schedule: "/schedule",
+    TransferRequest: "/transfer",
+    Walkins: "/walkins"
+}
+
+export default Pages
