a8aa8598b418d8a85977113c54080e533d690f45
jodh <jodh@itu.dk>
2021-11-17 19:25:57 +0100

    deleted the evil commetn

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -8,9 +8,6 @@ function Navigation () {
         <header>
             <div className="Navtabs">
                 <div className ="Navtab-Logo"><img src={logo} alt="ScanCar Logo" height="40px"></img></div>
-{/*                 <div className="Navtab">Booking Management</div>
-                <div className="Navtab">Rental Management</div> */}
-{/*                 <div className="navtab">Group Management</div> */}
                 <Navtab title="Booking Management" link={Pages.Schedule}/>
                 <Navtab title="Rental Management" link={Pages.DailyOverview}/>
                 <Navtab title="Group Management" link={Pages.RentalOffices}/>
