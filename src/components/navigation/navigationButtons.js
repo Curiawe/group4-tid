10a9f3b451573e45a48e2651d91c82ca24178ba6
mathildegeh <mathilde.gehring@hotmail.com>
2021-11-22 17:21:39 +0100

    component made reuseable ♻️

--- src/components/navigation/navigationButtons.js
+++ src/components/navigation/navigationButtons.js
@@ -2,21 +2,24 @@ import React from 'react';
 import './navigationButtons.css';
 import '../../index.css';
 
-
-function NavigationButtons(props) {
-    return (
+const NavButtons = (props) => {
+    const buttonOne = props.buttonOne;
+    const buttonTwo = props.buttonTwo;
+    const buttonThree = props.buttonThree;
+    
+    return ( 
         <div className="navButtons">
             <div className="button">
-                <h4>Rental Operations</h4>
+                <h4> { buttonOne } </h4>
             </div>
             <div className="button">
-                <h4>Rental Management</h4>
+                <h4> { buttonTwo } </h4>
             </div>
             <div className="button">
-                <h4>Group Management</h4>
+                <h4> { buttonThree } </h4>
             </div>
         </div>
     )
-}
+};
 
-export default NavigationButtons;
+export default NavButtons;
