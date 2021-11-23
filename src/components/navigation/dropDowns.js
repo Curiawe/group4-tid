b65c20468ff75fafe910d87fdbd7a28d3d08b9bf
mathildegeh <mathilde.gehring@hotmail.com>
2021-11-23 17:03:45 +0100

    dropdown menu made reuseable ♻️

---
+++ src/components/navigation/dropDowns.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../../index.css';
+
+const SelectCarGroup = () => {
+
+    return (
+        <div className="dropDown">
+            <select>
+                <option value="A – Small">A – Small</option>
+                <option value="B – Medium">B – Medium</option>
+                <option value="C – Large">C – Large</option>
+                <option value="D – X-Large">D – X-Large</option>
+                <option value="E – Family">E – Family</option>
+                <option value="F – Business">F – Business</option>
+                <option value="G – Station">G – Station</option>
+                <option value="H – Automatic">H – Automatic</option>
+                <option value="I – Small Automatic">I – Small Automatic</option>
+            </select>
+        </div>
+    )
+}
+
+const SelectPaymentMethod = () => {
+    return (
+        <div className="dropDown">
+            <select>
+                <option value="Select Payment Method">Select Payment Method</option>
+                <option value="Apple Pay">Apple Pay</option>
+                <option value="Credit Card">Credit Card</option>
+                <option value="Debit Card">Debit Card</option>
+                <option value="MobilePay">MobilePay</option>
+                <option value="PayPal">PayPal</option>
+            </select>
+        </div>
+    )
+}
+
+export { SelectCarGroup, SelectPaymentMethod }
