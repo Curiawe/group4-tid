0cb9c6efa221f49257bd45aa93a9f95f5302b55d
mathildegeh <mathilde.gehring@hotmail.com>
2021-11-24 23:51:57 +0100

    popup fixed

--- src/components/booking.js
+++ src/components/booking.js
@@ -187,7 +187,7 @@ function Buttons(props) {
             <button onClick={() => setButtonPopup(true)}>I'm a button</button>
             <PopupOneButton className="popupRed" buttonCenter="OK" trigger={buttonPopup} setTrigger={setButtonPopup}>
                     <p>here's the icon</p>
-                    <h3>here's some text about what you just did</h3>
+                    <p>here's some text about what you just did</p>
                     <p>here's how to move on</p>
             </PopupOneButton>
         </div>
