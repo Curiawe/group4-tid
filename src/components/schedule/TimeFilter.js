aeab2317b7bfa404123233e05f02e18c6fb64f0a
jodh <jodh@itu.dk>
2021-11-24 18:50:01 +0100

    timepicker credit attribution added

--- src/components/schedule/TimeFilter.js
+++ src/components/schedule/TimeFilter.js
@@ -1,4 +1,9 @@
 import DatePicker from 'sassy-datepicker';
+/**Datepicker credit: sassy-datepicker at
+ * https://www.npmjs.com/package/sassy-datepicker
+ * @license MIT (https://github.com/sassy-labs/datepicker/blob/main/LICENSE)
+ * @author SWiddharth Borderwala 2021
+ */
 import './schedule.css'
 
 export default function TimeFilter(props){
