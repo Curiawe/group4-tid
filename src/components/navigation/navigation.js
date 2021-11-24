bd45071205910f76e3c492bc6ddc4902e98a5132
mathildegeh <mathilde.gehring@hotmail.com>
2021-11-24 23:45:25 +0100

    last minute fixes to the sidebar

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,16 +1,18 @@
 import './navigation.css';
 import logo from './logo.png';
 import Pages from '../../pages/Pages';
-import PageGroups from '../navigation/PageGroups';
 import Navtab from './Navtab'
 import { Link } from 'react-router-dom';
+import '../../index.css'
 
 function Navigation () {
 
     return (
         <header>
             <div className="Navtabs">
-                <Link to={Pages.Home} className ="Navtab-Logo"><img src={logo} alt="ScanCar Logo" height="40px"/></Link>
+                <div className="Navtab-Logo">
+                    <Link to={Pages.Home} className="Navtab-Logo"><img src={logo} alt="ScanCar Logo" height="40px"/></Link>
+                </div>
                 <Navtab title="Booking Management" link={Pages.BookingLandingPage}/>
                 <Navtab title="Rental Management" link={Pages.RentalLandingPage}/>
                 <Navtab title="Group Management" link={Pages.GroupLandingPage}/>
