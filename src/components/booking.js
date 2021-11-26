145ee75d44a237be29d37d2909477c89cdab4fa5
mathildegeh <mathilde.gehring@hotmail.com>
2021-11-26 20:48:10 +0100

    placeholder kept while+after input + buttons

--- src/components/booking.js
+++ src/components/booking.js
@@ -6,6 +6,8 @@ import { SelectCarGroup, SelectPaymentMethod } from './dropDowns';
 import { PopupOneButton } from './popup';
 import { useState } from 'react';
 import './popup.css'
+import ButtonStyled from './buttons/ColorButton';
+import { COLORS } from '../values/colors';
 
 
 function BookingHeader() {
@@ -26,24 +28,27 @@ function BookingLogistics(props) {
         <div>
             <div className="rowLayout">
                 <h5>Pick-up</h5>
+             
                 <div className="location">
-                    <InputField className="inputField" placeHolder="Select Location"/>
-                    <SelectBox className="radioButton" type="radio" buttonText="Walk-in"/>
+                    <InputField className="inputField" type="text" placeHolder="Select Location"/>
+                    <SelectBox className="selectBox" type="checkbox" buttonText="Walk-in"/>
                 </div>
-                <div className="dateAndTime">
-                    <InputField className="inputField"  placeHolder="Select Date"/>
-                    <InputField className="inputField"  placeHolder="Select Time"/>
+                <div className=".columnLayout">
+                    <InputField className="inputField" type="date" placeHolder="Select Date"/>
+                    <InputField className="inputField" type="time" placeHolder="Select Time"/>
                 </div>
+            
             </div>
             <div className="rowLayout">
                 <h5>Return</h5>
                 <div className="location">
-                    <InputField className="inputField" placeHolder="Select Location"/>
+                    <InputField className="inputField" type="text" placeHolder="Select Location"/>
                 </div>
-                <div className="dateAndTime">
-                    <InputField className="inputField" placeHolder="Select Date"/>
-                    <InputField className="inputField" placeHolder="Select Time"/>
+                <div className="columnLayout">
+                    <InputField className="inputField" type="date" placeHolder="Select Date"/>
+                    <InputField className="inputField" type="time" placeHolder="Select Time"/>
                 </div>
+              
             </div>
         </div>
     )
@@ -74,11 +79,11 @@ function ExtraServices(props) {
             <div className="extraServicesSelect">
                 <div className="extraDriverSelect">
                     <SelectBox className="selectBox" type="checkbox" buttonText="Extra Driver"/>
-                    <div className="AddDriverBtn"> ADD DRIVER BTN </div>
+                    <ButtonStyled title="Add Driver" className="buttonSmall"/>
                 </div>
                 <div className="extraMileageSelect">
                     <SelectBox className="selectBox" type="checkbox" buttonText="Extra Mileage"/>
-                    <InputField className="inputField" placeHolder="Enter Extra Mileage"/>
+                    <InputField className="inputField" type="text" placeHolder="Enter Extra Mileage"/>
                 </div>
             </div>
         </div>
@@ -89,60 +94,18 @@ function CustomerInformation(props) {
     return (
         <div className="rowLayout">
             <h5>Customer Information</h5>
-            <div className="customerInfoFields">
-                    <div className="searchBox">
-                        <InputField className="inputField" placeHolder="Search Existing Customers"/>
-                    </div>
-                    <div className="customerInfoInput">
-                    <div className="info">
-                        <div className="infoType">
-                        Name:
-                        </div>
-                        <InputField className="inputField" placeHolder="Driver's Name"/>
-                    </div>
-                    <div className="info">
-                        <div className="infoType">
-                        Address:
-                        </div>
-                        <InputField className="inputField" placeHolder="Driver's Address"/>
-                    </div>
-                    <div className="info">
-                        <div className="infoType">
-                        Phone:
-                        </div>
-                        <InputField className="inputField" placeHolder="Driver's Phone Number"/>
-                    </div>
-                    <div className="info">
-                        <div className="infoType">
-                        Email:
-                        </div>
-                        <InputField className="inputField" placeHolder="Driver's Email Address"/>
-                    </div>
-                    <div className="info">
-                        <div className="infoType">
-                        Date of Birth:
-                        </div>
-                        <InputField className="inputField" placeHolder="dd/mm/yy"/>
-                    </div>
-                    <div className="info">
-                        <div className="infoType">
-                        License ID:
-                        </div>
-                        <InputField className="inputField" placeHolder="Driver's License ID"/>
-                    </div>
-                    <div className="info">
-                        <div className="infoType">
-                        License Issued:
-                        </div>
-                        <InputField className="inputField" placeHolder="dd/mm/yy"/>
-                    </div>
-                    <div className="info">
-                        <div className="infoType">
-                        License Expiration:
-                        </div>
-                        <InputField className="inputField" placeHolder="dd/mm/yy"/>
-                    </div>
-                </div>
+  
+            <div className="columnLayout">
+                        <InputField className="inputField" type="text" placeHolder="Search Existing Customers"/>
+                        <InputField className="inputField" type="text" placeHolder="Driver's Name"/>
+                        <InputField className="inputField" type="address" placeHolder="Driver's Address"/>
+                        <InputField className="inputField" type="tel" placeHolder="Driver's Phone Number"/>
+                        <InputField className="inputField" type="email" placeHolder="Driver's Email Address"/>
+                        <InputField className="inputField" type="date" placeHolder="Driver's Date of Birth"/>
+                        <InputField className="inputField" type="text" placeHolder="Driver's License ID"/>
+                        <InputField className="inputField" type="date" placeHolder="Licence Issue Date"/>
+                        <InputField className="inputField" type="date" placeHolder="License Expiration Date"/>
+              
             </div>
         </div>
     )
@@ -180,11 +143,13 @@ function PaymentMethod(props) {
     )
 }
 
-function Buttons(props) {
+function BookingButtons(props) {
     const [buttonPopup, setButtonPopup] = useState(false);
     return (
-        <div className="buttons">
-            <button onClick={() => setButtonPopup(true)}>I'm a button</button>
+        <div>
+            <ButtonStyled title="Delete Booking" className="buttonSmall"/>
+            <ButtonStyled title="Cancel Booking" className="buttonSmall"/>
+            <ButtonStyled title="Confirm Booking" onClick={() => setButtonPopup(true)}/>
             <PopupOneButton className="popupRed" buttonCenter="OK" trigger={buttonPopup} setTrigger={setButtonPopup}>
                     <p>here's the icon</p>
                     <p>here's some text about what you just did</p>
@@ -208,7 +173,7 @@ function Booking(props) {
             <CustomerInformation/>
             <HorizontalLine/>
             <PaymentMethod/>
-            <Buttons/>
+            <BookingButtons/>
 
             </div>
     );
