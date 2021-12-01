c5b78e9eee71859045e5106df344f167b0df1ba5
marinabjorns <90047932+marinabjorns@users.noreply.github.com>
2021-12-02 00:53:21 +0100

    We can now hover!

--- src/components/cards/CardsForBooking.js
+++ src/components/cards/CardsForBooking.js
@@ -3,6 +3,8 @@ import ButtonStyled from '../buttons/ColorButton';
 import { IconBody } from './IconBody';
 import Pages from '../../pages/Pages';
 import { Icon } from '@iconify/react';
+import '../booking.css';
+import InputField from '../inputField';
 
 /* Schema:
 000: {Name:"Per Son", Ref, Phone, PickupPlace, PickupTime, ReturnPlace, ReturnTime, CarGroup, ExtraService}
@@ -30,7 +32,6 @@ function BookingCard(props) {
     }
 
     return (
-
     <div className="card">
         <div className="cardBodyBooking">
             <div className="h4Booking"> BOOKING {booking.Ref} </div>
@@ -50,5 +51,6 @@ function BookingCard(props) {
     
     )
 }
+
 
 export default BookingCard
--- src/pages/BookingSearch.js
+++ src/pages/BookingSearch.js
@@ -1,16 +1,9 @@
-import '../components/cards/cards.css';
-import BookingCard from "../components/cards/CardsForBooking";
+import BookingOverviewCont from '../components/cards/BookingOverview';
+
 
 function BookingSearch () {
     return (
-        <div className="cardPageMargin">
-            <div className="cardMargin">
-                <BookingCard booking="814"/>
-            </div>
-            <div className="cardMargin">
-                <BookingCard booking="815"/>
-            </div>
-        </div>
+        <BookingOverviewCont />
     )
 }
 
