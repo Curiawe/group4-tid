ec23e19a4db93a6517060e7a78d86c432ff5e862
mathildegeh <mathilde.gehring@hotmail.com>
2021-12-01 17:06:15 +0100

    map-function removed from sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,13 +1,11 @@
 import './sidebar.css';
 import SidebarLinks from './SidebarLinks'
-import { mapToSidebarLink } from './SidebarLinks';
 import Pages from '../../pages/Pages';
 
 function Sidebar () {
     return (
         <nav className="SidebarContainer">
             <SidebarLinks/>
-            {mapToSidebarLink(Pages.BookingManagement)}
         </nav>
     )
 }
