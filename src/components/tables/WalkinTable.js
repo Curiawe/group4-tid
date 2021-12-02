9604569348440ebaab0f71c9b769b4b5dd5dcc5c
jodh <jodh@itu.dk>
2021-12-02 15:56:17 +0100

    working on automated table - next step: fixing dynamic access of table content from array

--- src/components/tables/WalkinTable.js
+++ src/components/tables/WalkinTable.js
@@ -7,6 +7,19 @@ import DatePicker from 'sassy-datepicker';
 //Data
 import { WALKINS } from '../../data/walkins'
 
+let walkins = []
+walkins.push(WALKINS['16.12.2021'])
+walkins.push(WALKINS['17.12.2021'])
+walkins.push(WALKINS['18.12.2021'])
+walkins.push(WALKINS['19.12.2021'])
+walkins.push(WALKINS['20.12.2021'])
+walkins.push(WALKINS['21.12.2021'])
+walkins.push(WALKINS['22.12.2021'])
+walkins.push(WALKINS['23.12.2021'])
+walkins.push(WALKINS['24.12.2021'])
+walkins.push(WALKINS['25.12.2021'])
+walkins.push(WALKINS['26.12.2021'])
+
 function WalkinHeader () {
     return (
         <thead>
@@ -27,11 +40,28 @@ function WalkinHeader () {
 
 function WalkinRow (props) {
     const rowDate = new Date (props.date).toLocaleDateString('da-DA', { year: 'numeric', month: '2-digit', day: '2-digit' })
+    //let bool = (walkins.find(rowDate))
+    console.log(walkins.find((el) => {
+        return (el.Date === props.date)
+    }))
+    if (!walkins.find((el) => {
+        return (el.Date === props.date)
+    })){// if the current date is not part of walkins, then
+        walkins.push({rowDate : {Date: props.date, estimate: 0, total: 0, changeable: true}}) 
+    }
+
+    // find the element in the array, then return the estimate
+    let estimate = (!walkins.find((el) => {
+        if (el.Date === props.date) {
+            return (el.estimate)
+        }
+    }))
+    let total = WALKINS[rowDate].total
     return (
     <tr>
         <td>{rowDate}</td>
-        <td>{WALKINS[1].estimate} </td>
-        <td>{WALKINS[1].total}</td>
+        <td>{estimate} </td>
+        <td>{total}</td>
     </tr>
     )
 
