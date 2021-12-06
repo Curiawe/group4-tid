e61bc4b99908576954c97a3f476e744cafe46e46
jodh <jodh@itu.dk>
2021-12-06 16:35:52 +0100

    daily overview tables created

---
+++ src/components/tables/DailyOverviewTable.js
@@ -0,0 +1,7 @@
+export default function DailyOverviewTable (props) {
+    return (
+        <table>
+            Hello
+        </table>
+    )
+}
--- src/pages/DailyOverview.js
+++ src/pages/DailyOverview.js
@@ -1,6 +1,12 @@
+import DailyOverviewTable from "../components/tables/DailyOverviewTable";
+
 function DailyOverview () {
     return (
+        <>
         <h1>Daily Overview</h1>
+        <DailyOverviewTable />
+        </>
+
     )
 }
 
