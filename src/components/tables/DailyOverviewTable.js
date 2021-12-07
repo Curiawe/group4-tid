2cb89f3c626bfcc7b43520c6eeac334f13ec2434
jodh <jodh@itu.dk>
2021-12-07 19:22:01 +0100

    Daily Overview has date selection box and 'jump to today' button

--- src/components/tables/DailyOverviewTable.js
+++ src/components/tables/DailyOverviewTable.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 export default function DailyOverviewTable (props) {
+
+    const [date, setDate] = useState(new Date());
+
     return (
-        <table>
-            Hello
-        </table>
+        <div>
+            <p id="large">Current Date: { (new Date(date)).toLocaleDateString() }</p>
+            <div style={{display:"flex", flexDirection:"row"}}>
+                <form style={{marginRight:"32px"}}>
+                    <input className = "inputField" type="date" onChange = {(e) => setDate(e.target.value)}></input>
+                </form>
+                <button onClick={(e) => setDate(new Date ())}>Jump to today</button>
+            </div>
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
     )
 }
