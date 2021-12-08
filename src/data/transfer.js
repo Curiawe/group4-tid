14d1fab89d8554c98e21f1fb47c14261029605ee
jodh <jodh@itu.dk>
2021-12-08 17:23:57 +0100

    made transfer an array with Date instead of string

--- src/data/transfer.js
+++ src/data/transfer.js
@@ -1,12 +1,12 @@
-import CARS from "cars.js"
-import LOCATIONS from "location.js"
+import {CARS} from "cars.js"
+import {LOCATIONS} from "location.js"
 
-export let TRANSFERS = {
-    1: {Incoming:LOCATIONS.CPC, Outgoing:LOCATIONS.CPA, Date:"25.11.2021", Car:CARS.JF94837},
-    2: {Incoming:LOCATIONS.CPC, Outgoing:LOCATIONS.CPA, Date:"25.11.2021", Car:CARS.FM23456},
-    3: {Incoming:LOCATIONS.CPA, Outgoing:LOCATIONS.CPC, Date:"25.11.2021", Car:CARS.IH12345},
-    4: {Incoming:LOCATIONS.AHN, Outgoing:LOCATIONS.AHA, Date:"25.11.2021", Car:CARS.HM23456},
-    5: {Incoming:LOCATIONS.AHA, Outgoing:LOCATIONS.AHC, Date:"25.11.2021", Car:CARS.HM12345},
-    6: {Incoming:LOCATIONS.AHA, Outgoing:LOCATIONS.AHS, Date:"25.10.2021", Car:CARS.IH23456},
-    7: {Incoming:LOCATIONS.AHS, Outgoing:LOCATIONS.AHN, Date:"26.10.2021", Car:CARS.FC56789}
-}
+export let TRANSFERS = [
+    {Incoming:LOCATIONS.CPC, Outgoing:LOCATIONS.CPA, Date:new Date.UTC(2021,11,8), Car:CARS.JF94837},
+    {Incoming:LOCATIONS.CPC, Outgoing:LOCATIONS.CPA, Date:new Date.UTC(2021,11,8), Car:CARS.FM23456},
+    {Incoming:LOCATIONS.CPA, Outgoing:LOCATIONS.CPC, Date:new Date.UTC(2021,11,8), Car:CARS.IH12345},
+    {Incoming:LOCATIONS.AHN, Outgoing:LOCATIONS.AHA, Date:new Date.UTC(2021,11,9), Car:CARS.HM23456},
+    {Incoming:LOCATIONS.AHA, Outgoing:LOCATIONS.AHC, Date:new Date.UTC(2021,11,9), Car:CARS.HM12345},
+    {Incoming:LOCATIONS.AHA, Outgoing:LOCATIONS.AHS, Date:new Date.UTC(2021,11,10), Car:CARS.IH23456},
+    {Incoming:LOCATIONS.AHS, Outgoing:LOCATIONS.AHN, Date:new Date.UTC(2021,11,8), Car:CARS.FC56789}
+]
