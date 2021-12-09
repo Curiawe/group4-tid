b975d0c907a40bc7183bc01ffcaf051e433b494d
jodh <jodh@itu.dk>
2021-12-10 00:58:10 +0100

    rental offices overview works now

--- src/data/locations.js
+++ src/data/locations.js
@@ -2,14 +2,14 @@
  * LOC: ["Location Name", "Address", "Email", "Phone","Region"]
  */
 
-export let LOCATIONS = {
-    CPC:["CPH City Center","Frørupvej 18, 1154 Københaven","CPC@scancar.dk", "00 00 00 00", "Hovedstaden"],
-    CPA:["CPH Airport", "Lufthavnsboulevarden 2, 2770 Kastrup","CPA@scancar.dk", "11 11 11 11", "Hovedstaden"],
-    AHC:["Aarhus Central", "Bygholms Alle 10, 8260 Viby J","AHC@scancar.dk", "22 22 22 22","Midtjylland"],
-    AHN:["Aarhus North", "Paludan-Müllers Vej 82, 8200 Aarhus N", "AHN@scancar.dk", "33 33 33 33","Midtjylland"],
-    FYS:["Fyn South", "Hal J, Frederiksø 2, 5700 Svendborg","FYA@scancar.dk", "77 77 77 77","Fyn"],
-    FYN:["Fyn North", "Gamle Havnekaj 3, 5000 Odense C","FYN@scancar.dk", "66 66 66 66","Fyn"],
-    AHS:["Aarhus South", "Moesgårdvej 11B, 8270 Højbjerg","AHS@scancar.dk", "44 44 44 44","Midtjylland"],
-    AHA:["Aarhus Harbor", "Værkmestergade 5, 8000 Aarhus","AHA@scancar.dk", "55 55 55 55","Midtjylland"],
-}
+export let LOCATIONS = [
+    {ID: "CPC", Location: "CPH City C enter", Address:"Frørupvej 18, 1154 Københaven", Email:"CPC@scancar.dk", Phone:"00 00 00 00", Region:"Hovedstaden"},
+    {ID: "CPA", Location: "CPH Airport", Address: "Lufthavnsboulevarden 2, 2770 Kastrup",Email:"CPA@scancar.dk", Phone:"11 11 11 11", Region:"Hovedstaden"},
+    {ID: "AHC", Location: "Aarhus Central", Address: "Bygholms Alle 10, 8260 Viby J",Email:"AHC@scancar.dk", Phone:"22 22 22 22",Region:"Midtjylland"},
+    {ID: "AHN", Location: "Aarhus North", Address: "Paludan-Müllers Vej 82, 8200 Aarhus N", Email:"AHN@scancar.dk", Phone:"33 33 33 33",Region:"Midtjylland"},
+    {ID: "FYS", Location: "Fyn South", Address: "Hal J, Frederiksø 2, 5700 Svendborg",Email:"FYA@scancar.dk", Phone:"77 77 77 77",Region:"Fyn"},
+    {ID: "FYN", Location: "Fyn North", Address: "Gamle Havnekaj 3, 5000 Odense C",Email:"FYN@scancar.dk", Phone:"66 66 66 66",Region:"Fyn"},
+    {ID: "AHS", Location: "Aarhus South", Address: "Moesgårdvej 11B, 8270 Højbjerg",Email:"AHS@scancar.dk", Phone:"44 44 44 44",Region:"Midtjylland"},
+    {ID: "AHA", Location: "Aarhus Harbor", Address: "Værkmestergade 5, 8000 Aarhus",Email:"AHA@scancar.dk", Phone:"55 55 55 55",Region:"Midtjylland"},
+]
 
