2167e1638f4cc8a291f5f3c87e1bf22d6cdc1c9e
mathildegeh <mathilde.gehring@hotmail.com>
2021-12-13 23:51:31 +0100

    modal for editing customer info

--- src/components/modals/returnComponents/customerInfo.js
+++ src/components/modals/returnComponents/customerInfo.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { Overlay } from "../pickupReturnTransferSkeleton";
+import CustomerInfoModal from "../customerInfoModal";
+import FeatherIcon from "feather-icons-react";
 
 function ReturnCustomerInfo(props) {
-  return <Overlay title="Customer Info">CONTENT</Overlay>;
+  const [showCustomerInfoModal, setShowCustomerInfoModal] = useState(false);
+  return (
+    <Overlay title="Customer Info">
+      here's some info
+      <div
+        className="clickableIcon"
+        onClick={() => setShowCustomerInfoModal(true)}
+      >
+        <FeatherIcon icon="edit" className="icon" />
+      </div>
+      <CustomerInfoModal
+        showCustomerInfoModal={showCustomerInfoModal}
+        onClose={() => setShowCustomerInfoModal(false)}
+        onConfirm={() => setShowCustomerInfoModal(false)}
+      ></CustomerInfoModal>
+    </Overlay>
+  );
 }
 
 export { ReturnCustomerInfo };
