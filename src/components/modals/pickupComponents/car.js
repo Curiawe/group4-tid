1261b756be72f376eef6d982ed7e3e744818115c
mathildegeh <mathilde.gehring@hotmail.com>
2021-12-13 23:54:54 +0100

    placeholder text for car row

--- src/components/modals/pickupComponents/car.js
+++ src/components/modals/pickupComponents/car.js
@@ -4,6 +4,7 @@ import FeatherIcon from "feather-icons-react";
 function SelectedCar(props) {
   return (
     <Overlay title="Car">
+      here's the car
       <FeatherIcon icon="edit" className="icon" />
     </Overlay>
   );
