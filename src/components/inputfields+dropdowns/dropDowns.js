af49c5293bb480940979722bb2ef00f8f5cadaf7
mathildegeh <mathilde.gehring@hotmail.com>
2021-12-13 17:03:13 +0100

    finally fixed the weird format for dropdown

--- src/components/inputfields+dropdowns/dropDowns.js
+++ src/components/inputfields+dropdowns/dropDowns.js
@@ -3,7 +3,6 @@ import "./inputFieldsAndDropdown.css";
 import { CARGROUPS } from "../../data/carGroups";
 import { LOCATIONS } from "../../data/locations";
 import { CARSTATES } from "../../data/carStates";
-import FeatherIcon from "feather-icons-react";
 
 const SelectCarGroup = (props) => {
   const groups = [
@@ -19,39 +18,35 @@ const SelectCarGroup = (props) => {
   ];
 
   return (
-    <div className="dropDown">
-      <select defaultValue={"Select Car Group"} onChange={props.onChange}>
-        <option value="Select Car Group" disabled>
-          Select Car Group
-        </option>
-        <option value={groups[0]}>{groups[0]}</option>
-        <option value={groups[1]}>{groups[1]}</option>
-        <option value={groups[2]}>{groups[2]}</option>
-        <option value={groups[3]}>{groups[3]}</option>
-        <option value={groups[4]}>{groups[4]}</option>
-        <option value={groups[5]}>{groups[5]}</option>
-        <option value={groups[6]}>{groups[6]}</option>
-        <option value={groups[7]}>{groups[7]}</option>
-        <option value={groups[8]}>{groups[8]}</option>
-      </select>
-    </div>
+    <select defaultValue={"Select Car Group"} onChange={props.onChange}>
+      <option value="Select Car Group" disabled>
+        Select Car Group
+      </option>
+      <option value={groups[0]}>{groups[0]}</option>
+      <option value={groups[1]}>{groups[1]}</option>
+      <option value={groups[2]}>{groups[2]}</option>
+      <option value={groups[3]}>{groups[3]}</option>
+      <option value={groups[4]}>{groups[4]}</option>
+      <option value={groups[5]}>{groups[5]}</option>
+      <option value={groups[6]}>{groups[6]}</option>
+      <option value={groups[7]}>{groups[7]}</option>
+      <option value={groups[8]}>{groups[8]}</option>
+    </select>
   );
 };
 
 const SelectPaymentMethod = (props) => {
   return (
-    <div className="dropDown">
-      <select defaultValue={"Select Payment Method"} onChange={props.onChange}>
-        <option value="Select Payment Method" disabled>
-          Select Payment Method
-        </option>
-        <option value="Apple Pay">Apple Pay</option>
-        <option value="Credit Card">Credit Card</option>
-        <option value="Debit Card">Debit Card</option>
-        <option value="MobilePay">MobilePay</option>
-        <option value="PayPal">PayPal</option>
-      </select>
-    </div>
+    <select defaultValue={"Select Payment Method"} onChange={props.onChange}>
+      <option value="Select Payment Method" disabled>
+        Select Payment Method
+      </option>
+      <option value="Apple Pay">Apple Pay</option>
+      <option value="Credit Card">Credit Card</option>
+      <option value="Debit Card">Debit Card</option>
+      <option value="MobilePay">MobilePay</option>
+      <option value="PayPal">PayPal</option>
+    </select>
   );
 };
 
@@ -59,21 +54,19 @@ const SelectLocation = (props) => {
   const locations = LOCATIONS;
 
   return (
-    <div className="dropDown">
-      <select defaultValue={"Select Location"} onChange={props.onChange}>
-        <option value="Select Location" disabled>
-          {props.dropdownTitle}
-        </option>
-        <option value={locations[0].Location}>{locations[0].Location}</option>
-        <option value={locations[1].Location}>{locations[1].Location}</option>
-        <option value={locations[2].Location}>{locations[2].Location}</option>
-        <option value={locations[3].Location}>{locations[3].Location}</option>
-        <option value={locations[4].Location}>{locations[4].Location}</option>
-        <option value={locations[5].Location}>{locations[5].Location}</option>
-        <option value={locations[6].Location}>{locations[6].Location}</option>
-        <option value={locations[7].Location}>{locations[7].Location}</option>
-      </select>
-    </div>
+    <select defaultValue={"Select Location"} onChange={props.onChange}>
+      <option value="Select Location" disabled>
+        {props.dropdownTitle}
+      </option>
+      <option value={locations[0].Location}>{locations[0].Location}</option>
+      <option value={locations[1].Location}>{locations[1].Location}</option>
+      <option value={locations[2].Location}>{locations[2].Location}</option>
+      <option value={locations[3].Location}>{locations[3].Location}</option>
+      <option value={locations[4].Location}>{locations[4].Location}</option>
+      <option value={locations[5].Location}>{locations[5].Location}</option>
+      <option value={locations[6].Location}>{locations[6].Location}</option>
+      <option value={locations[7].Location}>{locations[7].Location}</option>
+    </select>
   );
 };
 
@@ -81,18 +74,16 @@ const SelectCarState = (props) => {
   const carStates = CARSTATES;
 
   return (
-    <div className="dropDown">
-      <select defaultValue={"Select Car State"} onChange={props.onChange}>
-        <option value="Select Car State" disabled>
-          Select Car State
-        </option>
-        <option value={carStates[0]}>Ready</option>
-        <option value={carStates[1]}>Rented</option>
-        <option value={carStates[2]}>Returned</option>
-        <option value={carStates[3]}>Transfer</option>
-        <option value={carStates[4]}>Unaivailable</option>
-      </select>
-    </div>
+    <select defaultValue={"Select Car State"} onChange={props.onChange}>
+      <option value="Select Car State" disabled>
+        Select Car State
+      </option>
+      <option value={carStates[0]}>Ready</option>
+      <option value={carStates[1]}>Rented</option>
+      <option value={carStates[2]}>Returned</option>
+      <option value={carStates[3]}>Transfer</option>
+      <option value={carStates[4]}>Unaivailable</option>
+    </select>
   );
 };
 
