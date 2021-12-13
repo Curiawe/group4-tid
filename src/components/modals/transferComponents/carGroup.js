7eb8a19fc8ed48fb6c403bca21dd1698ad0eadd3
mathildegeh <mathilde.gehring@hotmail.com>
2021-12-13 13:50:11 +0100

    one more for transfer

---
+++ src/components/modals/transferComponents/carGroup.js
@@ -0,0 +1,15 @@
+import "../modal.css";
+import { SelectCarGroup } from "../../inputfields+dropdowns/dropDowns";
+import { Overlay } from "../pickupReturnTransferSkeleton";
+
+function TransferCarGroup(props) {
+  return (
+    <Overlay title="Car Group">
+      <SelectCarGroup
+        onChange={(e) => props.onChangeCarGroup(e.target.value)}
+      />
+    </Overlay>
+  );
+}
+
+export { TransferCarGroup };
