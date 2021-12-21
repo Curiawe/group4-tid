64773b3fc09e2ebd7e64fd4a7a295b3092798465
mathildegeh <mathilde.gehring@hotmail.com>
2021-12-21 10:54:05 +0100

    car cards show car group, seats and baggage again

--- src/components/cards/CardsForOverview.js
+++ src/components/cards/CardsForOverview.js
@@ -31,12 +31,12 @@ function LargeCardBody(props) {
     <div className="card" onClick={(e) => handleClick(e)}>
       <img className="cardImg" src={car.Image} alt="carpic" />
       <div className="h4Large"> {car.Model} </div>
-      <div className="p1Large"> {usedCarGroup[0]}</div>
+      <div className="p1Large"> {usedCarGroup.name}</div>
       <div className="p2"> {car.License} </div>
       <div className="cardBodyLarge">
         <div className="iconRow">
-          <IconBody title={usedCarGroup[1]} icon="user" />
-          <IconBody title={usedCarGroup[2]} icon="briefcase" />
+          <IconBody title={usedCarGroup.Seats} icon="user" />
+          <IconBody title={usedCarGroup.Baggage} icon="briefcase" />
           <div className="iconBody">
             <div className="mainIcons">
               <Icon icon="mdi:car-door" width="23.07px" height="25.01px" />
@@ -51,7 +51,7 @@ function LargeCardBody(props) {
           <ColorIcon title={car.Color} />
         </div>
         <div className="iconRow">
-          <IconBody title={car.Location[0]} icon="map-pin" />
+          <IconBody title={car.Location.Location} icon="map-pin" />
         </div>
         <div className="iconRow">
           <ColorIcon title={car.Status} />
