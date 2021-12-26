1b9d77aad5740d0f8347d81fbe5a84afc5eb1221
marinabjorns <90047932+marinabjorns@users.noreply.github.com>
2021-12-26 10:21:11 +0100

    Code actually works right now!

--- src/components/cards/SelectCar.js
+++ src/components/cards/SelectCar.js
@@ -1,40 +1,43 @@
 import { CARS } from "../../data/cars";
 import { BOOKINGS } from "../../data/bookings";
-import { Filter } from "react-feather";
+import { Filter, X } from "react-feather";
 import { TRANSFERS } from "../../data/transfer";
+import { CARGROUPS } from "../../data/carGroups";
 // group, availability
 
-function FilterCar(location, status, group) {
+
+// find all cars with location and available status
+function FilterCar(location, status) {
     return (
-    CARS.filter(car =>  car.Location.Location === location && car.Status === status && car.carGroup === group).map(filteredCar => (
-        [filteredCar.License]
+    CARS.filter(car =>  car.Location.Location === location && car.Status === status).map(filteredCar => (
+        filteredCar
     )))
 }
 
 function FilterTransferCars(cars, pickupTime, returnTime) {
-    let transfers = TRANSFERS[0];
-
-    TRANSFERS.map((trans) => {
-        if (trans.Car.License === cars) {
-        transfers = trans;
-        }
-        return null;
-        });
-        
-    if (transfers.Car.License.includes(cars)) {
     
-        if (pickupTime < transfers.Date && returnTime > transfers.Date){
-            return null
-            } 
-        else return transfers.Car.License
-    }
+    let avCars = cars
+    let transferCars = []
 
-    else return cars
+    TRANSFERS.map(t => {if (pickupTime <= t.Date && returnTime >= t.Date){
+        transferCars.push(t.Car)
+        return null
+    }})
+
+    let finalCars = avCars.filter(c => !transferCars.includes(c))
+    let licenses = []
+    finalCars.map(c => licenses.push(c.License))
+
+    return(
+        licenses
+    )
 }
 
 
+
+
 function ShowAvailableCars(bookingRef) {
-    
+
     let booking = BOOKINGS[0];
 
     BOOKINGS.map((bkng) => {
@@ -48,11 +51,10 @@ function ShowAvailableCars(bookingRef) {
     let carGroup = booking.carGroup
     let carStatus = "Available"
 
-    let filteredCars = FilterCar(pickupLocation, carStatus, carGroup).toString();
-
+    let filteredCars = FilterCar(pickupLocation, carStatus)
     let cars = FilterTransferCars(filteredCars, booking.Pickup.time, booking.Return.time)
 
     return (cars)
 } 
 
-export default ShowAvailableCars
+export default ShowAvailableCars 
