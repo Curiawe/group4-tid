b81d7b9b7b18377f83e3a9683389459cdab7f7f0
mathildegeh <mathilde.gehring@hotmail.com>
2021-12-30 14:44:12 +0100

    layout

--- src/components/modals/bookingComponents/extraServices.js
+++ src/components/modals/bookingComponents/extraServices.js
@@ -5,19 +5,21 @@ import { CheckBox } from "../../selectionBoxes/selectionBoxes";
 function ExtraServices(props) {
   return (
     <BookingComponent title="Extra Services">
-      <CheckBox
-        className="checkBox"
-        type="checkbox"
-        checked={props.extraDriver}
-        onChange={(e) => props.onChangeExtraDriver(e.target.checked)}
-        buttonText="Extra Driver"
-      />
-      <CheckBox
-        name="Mileage"
-        buttonText="Extra Mileage"
-        checked={props.extraMileage}
-        onChange={(e) => props.onChangeExtraMileage(e.target.checked)}
-      />
+      <div>
+        <CheckBox
+          className="checkBox"
+          type="checkbox"
+          checked={props.extraDriver}
+          onChange={(e) => props.onChangeExtraDriver(e.target.checked)}
+          buttonText="Extra Driver"
+        />
+        <CheckBox
+          name="Mileage"
+          buttonText="Extra Mileage"
+          checked={props.extraMileage}
+          onChange={(e) => props.onChangeExtraMileage(e.target.checked)}
+        />
+      </div>
     </BookingComponent>
   );
 }
