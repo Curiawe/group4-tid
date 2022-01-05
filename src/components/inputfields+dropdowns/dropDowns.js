76520091b195b6137e0f0df48f01c67db6f19475
mathildegeh <mathilde.gehring@hotmail.com>
2022-01-05 17:53:06 +0100

    payment method not needed anymore, time is

--- src/components/inputfields+dropdowns/dropDowns.js
+++ src/components/inputfields+dropdowns/dropDowns.js
@@ -30,21 +30,6 @@ const SelectCarGroup = (props) => {
   );
 };
 
-const SelectPaymentMethod = (props) => {
-  return (
-    <select defaultValue={"Select Payment Method"} onChange={(e) => props.onChange(e.target.value)}>
-      <option value="Select Payment Method" disabled>
-        Select Payment Method
-      </option>
-      <option value="Apple Pay">Apple Pay</option>
-      <option value="Credit Card">Credit Card</option>
-      <option value="Debit Card">Debit Card</option>
-      <option value="MobilePay">MobilePay</option>
-      <option value="PayPal">PayPal</option>
-    </select>
-  );
-};
-
 const SelectLocation = (props) => {
   const locations = LOCATIONS;
 
@@ -82,4 +67,31 @@ const SelectCarState = (props) => {
   );
 };
 
-export { SelectCarGroup, SelectPaymentMethod, SelectLocation, SelectCarState };
+const SelectTime = (props) => {
+  const OpeningHours = {
+    8: "08:00",
+    10: "10:00",
+    12: "12:00",
+    14: "14:00",
+    16: "16:00",
+    18: "18:00",
+    20: "20:00",
+  };
+
+  return (
+    <select defaultValue={props.defaultValue} onChange={props.onChange}>
+      <option value="Select Time" disabled>
+        Select Time
+      </option>
+      <option value={OpeningHours[0]}>08:00</option>
+      <option value={OpeningHours[1]}>10:00</option>
+      <option value={OpeningHours[2]}>12:00</option>
+      <option value={OpeningHours[3]}>14:00</option>
+      <option value={OpeningHours[4]}>16:00</option>
+      <option value={OpeningHours[5]}>18:00</option>
+      <option value={OpeningHours[6]}>20:00</option>
+    </select>
+  );
+};
+
+export { SelectCarGroup, SelectLocation, SelectCarState, SelectTime };
