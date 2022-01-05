309724d4ce16b6aa899e54f9ec81e91bf48a6c90
mathildegeh <mathilde.gehring@hotmail.com>
2022-01-05 17:47:01 +0100

    mileage explanation in "Your ScanCar"

--- src/components/tables/CarGroupTable.js
+++ src/components/tables/CarGroupTable.js
@@ -1,45 +1,46 @@
 import { CARGROUPS } from "../../data/carGroups";
-import './tables.css';
+import "./tables.css";
 
 function makeRows() {
-    let rows = []
-    CARGROUPS.map((group) => {
-        let row = (
-            <tr key = {group.name}>
-                <td>{group.name}</td>
-                <td>{group.Seats}</td>
-                <td>{group.Baggage}</td>
-                <td>{group.Price}</td>
-            </tr>
-        )
-        rows.push(row)
-        return null
-    })
+  let rows = [];
+  CARGROUPS.map((group) => {
+    let row = (
+      <tr key={group.name}>
+        <td>{group.name}</td>
+        <td>{group.Seats}</td>
+        <td>{group.Baggage}</td>
+        <td>{group.Price}</td>
+      </tr>
+    );
+    rows.push(row);
+    return null;
+  });
 
-    return rows
-
+  return rows;
 }
 
 function CarGroupTable() {
-
-
-
-    return (
-        <table style={{marginBottom:"32px"}} >
+  return (
+    <>
+      <table>
         <thead>
-            <tr>
-                <th>Car Group</th>
-                <th>Seats</th>
-                <th>Baggage Capacity</th>
-                <th>Hourly Price</th>
-            </tr>
+          <tr>
+            <th>Car Group</th>
+            <th>Seats</th>
+            <th>Baggage Capacity</th>
+            <th>Hourly Price</th>
+          </tr>
         </thead>
-        <tbody>
-        {makeRows()}
-        </tbody>
-    </table>
-)
-
+        <tbody>{makeRows()}</tbody>
+      </table>
+      <p style={{ fontSize: "12px" }}>
+        *All ScanCar cars come with 100 km per hour for up to 6 hours per day.
+        Any additional mileage can be bought for 2 DKK/km when placing the
+        booking. If the mileage has exceeded the mileage included in the booking
+        upon return, the extra mileage will be charged 20 DKK/km.
+      </p>
+    </>
+  );
 }
 
-export default CarGroupTable
+export default CarGroupTable;
