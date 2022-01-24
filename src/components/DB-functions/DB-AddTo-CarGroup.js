e188cbb75da6c094584fda0c214b9357a9c2bae3
mathildegeh <mathilde.gehring@hotmail.com>
2022-01-24 17:17:51 +0100

    rest api not working

--- src/components/DB-functions/DB-AddTo-CarGroup.js
+++ src/components/DB-functions/DB-AddTo-CarGroup.js
@@ -1,33 +1,39 @@
-import Parse from "parse";    
-    
-    /**
-     * 
-     * @param {String} groupName 
-     * @param {int} groupBaggage 
-     * @param {double} groupSeats 
-     * @param {int} groupPrice 
-     */
-export default async function addCarGroup(groupName, groupBaggage, groupSeats, groupPrice) {
-        const carGroup = new Parse.Object("CarGroups"); // give us the blueprint for CarGroup Objects
+import Parse from "parse";
 
-        // Setting our values
-        carGroup.set("name", groupName);
-        carGroup.set("seats", groupSeats);
-        carGroup.set("baggage", groupBaggage)
-        carGroup.set("price", groupPrice)
-        
-        try { // please try to do the things in curly bracket.
-            let result = await carGroup.save()
-            alert('New object created with objectId: ' + result.id);
-            } catch(error) { // but if any of this happens...
-                alert('Failed to create new object, with error code: ' + error.message); // do this instead
-            } finally { // no matter what, do that when you're done
-              
-            }
-    }
+/**
+ *
+ * @param {String} groupName
+ * @param {int} groupBaggage
+ * @param {double} groupSeats
+ * @param {int} groupPrice
+ */
+export default async function addCarGroup(
+  groupName,
+  groupBaggage,
+  groupSeats,
+  groupPrice
+) {
+  const carGroup = new Parse.Object("CarGroups"); // give us the blueprint for CarGroup Objects
 
-  
-  //Reading your First Data Object from Back4App
+  // Setting our values
+  carGroup.set("name", groupName);
+  carGroup.set("seats", groupSeats);
+  carGroup.set("baggage", groupBaggage);
+  carGroup.set("price", groupPrice);
+
+  try {
+    // please try to do the things in curly bracket.
+    let result = await carGroup.save();
+    alert("New object created with objectId: " + result.id);
+  } catch (error) {
+    // but if any of this happens...
+    alert("Failed to create new object, with error code: " + error.message); // do this instead
+  } finally {
+    // no matter what, do that when you're done
+  }
+}
+
+//Reading your First Data Object from Back4App
 /*   async function retrievePerson() {
     const query = new Parse.Query("Person");
     
